60fdde85cfe736ab642288d85ef4f921da02bdca
capitalgerald <capitalgerald@gmail.com>
2022-08-31 12:29:44 -0700

    Initial commit

---
+++ application/views/home.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Book Store - New Vision</title>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>my_custom/bootstrap/css/bootstrap-4.1.0.min.css">
+	<script type="text/javascript" src="<?php echo base_url(); ?>my_custom/jquery/jquery-3.6.0.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>my_custom/bootstrap/js/bootstrap-4.1.0.min.js"></script>	
+
+	<!-- fontawesome-free-6.1.2-web -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>my_custom/fontawesome-free-6.1.2-web/css/all.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>my_custom/fontawesome-free-6.1.2-web/css/fontawesome.min.css">
+
+	<style type="text/css">
+	</style>
+</head>
+<body>
+
+	<h1>Hi, Welcome to New Vision Book Store</h1>
+</body>
+</html>
